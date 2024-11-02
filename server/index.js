97bfb299d7e94c3cba422e49f864332d282cd9b6
tnvr-ahmd <thanveerahamed1100@gmail.com>
2024-11-02 17:47:56 +0530

    4

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors())
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:3000/,https://chat-app-seven-henna-32.vercel.app/",
+        origin:["http://localhost:3000/","https://chat-app-seven-henna-32.vercel.app/"],
         methods:["GET","POST"],
     },
 })
