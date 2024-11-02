85d98f38f3caaa517e72cdc506023be982c68378
tnvr-ahmd <thanveerahamed1100@gmail.com>
2024-11-02 11:20:32 +0530

    url update

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(cors())
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:3000, https://chat-application-liart-one.vercel.app/",
+        origin:"https://chat-application-liart-one.vercel.app/",
         methods:["GET","POST"],
     },
 })
